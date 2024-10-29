0565e4d09fab6837c4fe82e499534d2d2f5fcc97
juanposada25 <juandiegoposada245@gmail.com>
2024-10-28 23:46:23 -0500

    primeros pasos

---
+++ SupermarketWEB/Data/SupermarketContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarkerEF.Data
+{
+    internal class SupermarketContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+
+    }
+}
---
+++ SupermarketWEB/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using SupermarketWEB.Models;
+using Microsoft.Identity.Client;
+
+
+namespace SupermarketWEB.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
