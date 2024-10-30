58927fae35f84ccab50e8c8ac501098dd4d96f1c
juanposada25 <juandiegoposada245@gmail.com>
2024-10-30 00:05:34 -0500

    index finalizado

--- SupermarketWEB/Pages/PayModes/Index.cshtml.cs
+++ SupermarketWEB/Pages/PayModes/Index.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SupermarkerEF.Data;
 using SupermarketWEB.Models;
 
-namespace SupermarketWEB.Pages.Categories
+namespace SupermarketWEB.Pages.PayModes
 {
     public class IndexModel : PageModel
     {
@@ -15,13 +15,13 @@ namespace SupermarketWEB.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Categories { get; set; } = default!;
+        public IList<PayMode> PayModes { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.Categories != null)
+            if (_context.PayModes != null)
             {
-                Categories = await _context.Categories.ToListAsync();
+                PayModes = await _context.PayModes.ToListAsync();
             }
 
         }
